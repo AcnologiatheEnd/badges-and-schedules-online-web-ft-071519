ff46e1deb8fcf9e74ec0934daf34154b5124f5c5
Winston  Chung <18.chung.winston@ctkprep.org>
2019-07-20 00:26:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(arg)
+  "Hello, my name is #{arg}."
+end
+
+def batch_badge_creator(array)
+  new_m=[]
+  array.each do |name|
+    new_m.push("Hello, my name is #{name}.")
+  end
+  new_m
+end
+
+def assign_rooms(array)
+  msg=[]
+  array.each do |name|
+    msg.push("Hello, #{name}! You'll be assigned to room #{name.index+1}!")
+  end
+  msg
+end
+
